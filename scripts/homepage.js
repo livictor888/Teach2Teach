caa56d10f70d2490f378769c5e4c978416988022
Huynh Nhan Ngo <ngohuynhnhan@gmail.com>
2021-03-26 09:47:48 -0700

    Add new template for new post

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -1,5 +1,6 @@
 (function () {
-  window.onload = function () {
+  window.onload = async function () {
+    /**** ===== Navigate to post detail ===== ****/
     const postTitle = document.querySelectorAll(".post-title");
     const postReadmore = document.querySelectorAll(".post-readmore");
     const postContent = document.querySelectorAll(".post-content");
@@ -17,5 +18,22 @@
     postContent.forEach((elem) => {
       elem.addEventListener("click", navigateToPostDetail);
     });
+
+    /**** ===== Detect if user logged in ===== ****/
+    const loggedIn = await new Promise((res) => {
+      firebase.auth().onAuthStateChanged((user) => res(!!user));
+    });
+
+    /**** ===== Detect if user logged in ===== ****/
+    const addNewPostIcon = document.querySelector("#bottom-icon-add");
+    if (addNewPostIcon) {
+      addNewPostIcon.addEventListener("click", function () {
+        if (loggedIn) {
+          window.location.assign("new-post.html");
+        } else {
+          window.location.assign("login.html");
+        }
+      });
+    }
   };
 })();
