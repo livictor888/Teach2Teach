792716df32209302188e773a6f8a09d49a77b84a
Huynh Nhan Ngo <ngohuynhnhan@gmail.com>
2021-03-26 16:43:33 -0700

    Add loading state for navigating to post detail

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -71,9 +71,16 @@
       // Content
       const postContentElem = document.createElement("div");
       postContentElem.setAttribute("class", "my-2 post-content");
+      const formattedContent = shortenContent(post.content);
       postContentElem.innerHTML = `
-        <div class="my-2 post-content">
-        <p><span>${post.content}</span></p>
+        <p>${formattedContent}
+          ${
+            formattedContent.length < post.content.length &&
+            `<span class="text-read-more text-primary post-readmore">
+                Read More
+              </span>`
+          }
+        </p>
       `;
       container.appendChild(postContentElem);
 
@@ -101,71 +108,12 @@
       container.parentNode.appendChild(line);
     }
 
-    /**** ===== Helper functions ===== ****/
-    function renderNewPost(post) {
-      const postHomePage = document.querySelector("#homepage-content");
-
-      // Container
-      const container = document.createElement("div");
-      container.setAttribute("class", "container-fluid my-3");
-      postHomePage.appendChild(container);
-      container.addEventListener("click", () => {
-        window.location.href = `./post-detail.html?post_id=${post.id}`;
-      });
-
-      // Title
-      const postTitleElem = document.createElement("div");
-      postTitleElem.setAttribute("class", "h4 post-title");
-      postTitleElem.innerText = post.title;
-      container.appendChild(postTitleElem);
-
-      // Tag Badges
-      const tagContainer = document.createElement("div");
-      tagContainer.setAttribute("class", "d-flex mb-2");
-      tagContainer.innerHTML = `
-        <div class="badge rounded-pill bg-secondary text-white mr-1">
-          lorem ipsum
-        </div>
-        <div class="badge rounded-pill bg-secondary text-white mr-1">
-          lorem ipsum
-        </div>
-        <div class="badge rounded-pill bg-secondary text-white mr-1">
-          lorem ipsum
-        </div>
-      `;
-      container.appendChild(tagContainer);
-
-      // Content
-      const postContentElem = document.createElement("div");
-      postContentElem.setAttribute("class", "my-2 post-content");
-      postContentElem.innerHTML = `
-        <div class="my-2 post-content">
-        <p><span>${post.content}</span></p>
-      `;
-      container.appendChild(postContentElem);
-
-      // Interaction
-      const interactionElem = document.createElement("div");
-      interactionElem.setAttribute("class", "d-flex");
-      interactionElem.innerHTML = `
-          <div class="d-flex align-items-center">
-            <img class="icon" src="./images/icon-like.png" alt="icon-sharing" />
-            <p class="ml-2">12 Likes</p>
-          </div>
-          <div class="d-flex align-items-center ml-3 flex-1">
-            <img class="icon" src="./images/icon-comment.png" alt="icon-sharing" />
-            <p class="ml-2">254 comments</p>
-          </div>
-          <div class="d-flex align-items-center">
-            <img class="icon-bookmark" src="./images/icon-bookmark.png" alt="icon-bookmark" />
-          </div>
-      `;
-      container.appendChild(interactionElem);
-
-      // Separated line
-      const line = document.createElement("div");
-      line.setAttribute("class", "bg-secondary w-100 separated-line");
-      container.parentNode.appendChild(line);
+    function shortenContent(content) {
+      if (content.length >= 300) {
+        return content.substr(0, 300) + "...";
+      } else {
+        return content;
+      }
     }
   };
 })();
--- scripts/post-detail.js
+++ scripts/post-detail.js
@@ -3,16 +3,120 @@
     const urlParams = new URLSearchParams(window.location.search);
     const postId = urlParams.get("post_id");
 
-    const postTitleElem = document.querySelector("#post-title");
-    const postContentElem = document.querySelector("#post-content");
-
+    showSpinner();
     db.collection("posts")
       .doc(postId)
       .onSnapshot((doc) => {
-        postTitleElem.innerText = doc.data().title;
-        postContentElem.innerText = doc.data().content;
+        removeSpinner();
+        renderPostDetail(doc.data());
       });
   };
+
+  //
+  function renderPostDetail(post) {
+    const container = document.querySelector("#post-detail-container");
+    const contentElem = document.createElement("div");
+    contentElem.setAttribute("class", "container-fluid my-3");
+    contentElem.innerHTML = `
+      <!-- Title -->
+      <div class="h4">${post.title}</div>
+
+      <!-- Tag badges -->
+      <div class="d-flex mb-2">
+        <div class="badge rounded-pill bg-secondary text-white mr-1">
+          lorem ipsum
+        </div>
+        <div class="badge rounded-pill bg-secondary text-white mr-1">
+          lorem ipsum
+        </div>
+        <div class="badge rounded-pill bg-secondary text-white mr-1">
+          lorem ipsum
+        </div>
+      </div>
+
+      <!-- Rating, views and sharing -->
+      <div class="d-flex justify-content-between">
+        <div class="d-flex align-items-center">
+          <div class="d-flex align-items-center">
+            <ul class="list-inline rating-list">
+              <li><i class="fa fa-star yellow"></i></li>
+              <li><i class="fa fa-star yellow"></i></li>
+              <li><i class="fa fa-star yellow"></i></li>
+              <li><i class="fa fa-star yellow"></i></li>
+              <li><i class="fa fa-star gray"></i></li>
+            </ul>
+          </div>
+          <div>
+            <p class="small pl-3">3456 Reviews</p>
+          </div>
+        </div>
+        <div>
+          <img class="icon" src="./images/icon-sharing.png" alt="icon-sharing" />
+        </div>
+      </div>
+
+      <!-- Content -->
+      <div class="my-2">${post.content}</div>
+
+      <!-- Post interaction -->
+      <div class="d-flex">
+        <div class="d-flex align-items-center">
+          <img class="icon" src="./images/icon-like.png" alt="icon-sharing" />
+          <p class="ml-2">12 Likes</p>
+        </div>
+        <div class="d-flex align-items-center ml-3 flex-1">
+          <img class="icon" src="./images/icon-comment.png" alt="icon-sharing" />
+          <p class="ml-2">254 comments</p>
+        </div>
+        <div class="d-flex align-items-center">
+          <img class="icon-bookmark" src="./images/icon-bookmark.png" alt="icon-bookmark" />
+        </div>
+      </div>
+      <div class="bg-secondary w-100 mt-3 mb-2" style="height: 2px"></div>
+
+      <!-- Comment section -->
+      <div class="d-flex">
+        <img class="comment-avatar" src="https://randomuser.me/api/portraits/thumb/women/45.jpg" alt="avatar" />
+        <div class="ml-2">
+          <p>
+            <span class="comment-username">John</span> Lorem ipsum dolor, sit
+            amet consecur adipisicing elit.
+          </p>
+          <div class="d-flex comment-interactions">
+            <div class="mr-2">1 d</div>
+            <div class="mr-2">4 likes</div>
+            <div>Reply</div>
+          </div>
+        </div>
+      </div>
+
+      <!-- New comment section -->
+      <div class="w-100 my-4">
+        <input class="w-100 comment-input" placeholder="Comment here" />
+      </div>
+
+      <!-- Recommended Topics -->
+      <div class="mb-4">
+        <div class="h5">Recommended Topics</div>
+        <div class="d-flex rec-topics-container">
+          <div class="rec-topics">
+            <p>Topic 1</p>
+          </div>
+          <div class="rec-topics">
+            <p>Topic 2</p>
+          </div>
+          <div class="rec-topics">
+            <p>Topic 3</p>
+          </div>
+          <div class="rec-topics">
+            <p>Topic 4</p>
+          </div>
+        </div>
+      </div>
+    `;
+
+    container.appendChild(contentElem);
+  }
 })();
 
 // var current_star_statusses = [];
