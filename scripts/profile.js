0c151c464a6c50ed8ee9461bc929030de51bcc4b
Victor Li <vli30@my.bcit.ca>
2021-03-26 17:29:31 -0700

    implemented profile page

---
+++ scripts/profile.js
@@ -0,0 +1,90 @@
+// show profile page items: email, name, website url, biography
+
+isUserLoggedIn().then(function (somebody) {
+    if (somebody) {
+        console.log(somebody.uid);
+        db.collection("users")
+            .doc(somebody.uid)
+            .get()      //read the data
+            .then(function (doc) {
+                console.log(doc.data().name);
+                var userName = doc.data().name;
+                var email = doc.data().email;
+                document.getElementById("name").setAttribute("placeholder", userName)
+                document.getElementById("email").setAttribute("placeholder", email)
+            })
+    }
+})
+function UserProfileInformation() {
+    // const urlParams = new URLSearchParams(window.location.search);
+    // const userId = urlParams.get("user_id");
+    // const usernameElem = urlParams.get("#name");
+    // const userEmailElem = urlParams.get("#email");
+
+
+
+
+}
+
+
+// Edit Profile Button
+function letUserEdit() {
+    document.getElementById("name").removeAttribute("readonly")
+    document.getElementById("email").removeAttribute("readonly")
+    document.getElementById("bio").removeAttribute("readonly")
+    document.getElementById("EditProfileButton").style.visibility = "hidden"
+    document.getElementById("UpdateProfileButton").style.visibility = "visible"
+}
+
+// Update Button
+function letUserUpdate(){
+    document.getElementById("name").readOnly = true;
+    document.getElementById("email").readOnly = true;
+    document.getElementById("bio").readOnly = true;
+
+    //update to firebase
+    var updatedName = document.getElementById("name").value;
+    console.log(updatedName)
+    var updatedEmail = document.getElementById("email").value;
+    console.log(updatedEmail)
+    var updatedBio = document.getElementById("bio").value;
+    isUserLoggedIn().then(function (somebody) {
+        if (somebody) {
+            console.log(somebody.uid);
+            db.collection("users")
+                .doc(somebody.uid)
+                .set({
+                    name: updatedName,
+                    email: updatedEmail,
+                    bio: updatedBio
+                })      //write the new name into Firebase
+
+        }
+    })
+
+    // var user = firebase.auth().currentUser;
+    // user.updateProfile({
+    //     name: updatedName
+    // }).then(function() {
+    //     console.log("update successful")
+        
+    // })
+
+
+
+    // show spinner
+    // then make input fields readonly again and show edit button
+    document.getElementById("EditProfileButton").style.visibility = "visible"
+    document.getElementById("UpdateProfileButton").style.visibility = "hidden"
+    // removeSpinner()
+
+
+
+
+}
+
+
+
+
+
+// on Edit button click, remove all the readonly attributes from each form
