4000d28111810886cf2fe350a36c344a634c4ff5
Huynh Nhan Ngo <ngohuynhnhan@gmail.com>
2021-04-02 19:12:41 -0700

    Support multiple tag when create new post

--- scripts/new-post.js
+++ scripts/new-post.js
@@ -22,16 +22,57 @@
       showSpinner();
     }
 
+    /**** ===== Add new feature post add tag ===== ****/
+    const selectPostTag = document.querySelector("#new-post-tag");
+    const selectPostTagWrapper = document.querySelector(".tag-chosen");
+    if (selectPostTag && selectPostTagWrapper) {
+      selectPostTag.addEventListener("change", (event) => {
+        const selectedValue = event.target.value;
+        const currentSelectedValue =
+          selectPostTagWrapper.getAttribute("data-chosen") || "";
+
+        if (currentSelectedValue.indexOf(selectedValue) >= 0) {
+          // Remove tag bubble
+          selectPostTagWrapper.querySelectorAll("span").forEach((elem) => {
+            if (elem.innerText === selectedValue) {
+              elem.remove();
+            }
+            const newDataChosen = currentSelectedValue
+              .split(",")
+              .filter((val) => !!val && val !== selectedValue)
+              .join(",");
+            selectPostTagWrapper.setAttribute("data-chosen", newDataChosen);
+          });
+        } else {
+          // Create tag bubble
+          const tagElem = document.createElement("span");
+          tagElem.setAttribute("class", "tag-bubble");
+          tagElem.innerText = ` ${selectedValue} `;
+          selectPostTagWrapper.appendChild(tagElem);
+          selectPostTagWrapper.setAttribute(
+            "data-chosen",
+            currentSelectedValue + `,${selectedValue}`
+          );
+        }
+
+        // Reset
+        event.target.value = "";
+      });
+    }
+
     /**** ===== Add new feature upload post ===== ****/
     const newPostSubmitButton = document.querySelector("#new-post-submit-btn");
     newPostSubmitButton.addEventListener("click", async function () {
       const postTitle = document.querySelector("#new-post-title").value;
       const postContent = document.querySelector("#new-post-content").value;
-      const postTag = document.querySelector("#new-post-tag").value;
+      let postTagData = document
+        .querySelector(".tag-chosen")
+        .getAttribute("data-chosen");
+      const postTag = (postTagData || "").split(",").filter((val) => !!val);
 
       toggleError("new-post-title", postTitle);
       toggleError("new-post-content", postContent);
-      toggleError("new-post-tag", postTag);
+      toggleError("tag-wrapper", postTag.length > 0);
 
       if (postTitle && postContent) {
         await uploadNewPost({
