b967edcedc63c8c8bf7b7e1d9e2c72b4511c10a8
Hyerim_Shin <sinhyerim@gmail.com>
2021-04-07 16:34:52 -0700

    Implemented search function

--- scripts/search.js
+++ scripts/search.js
@@ -1,8 +1,73 @@
 const searchButton = document.getElementById("search-post");
+var showresult = document.querySelector("#result-here");
+var inputTextField = document.querySelector("#search-result");
 
 searchButton.addEventListener("click", function () {
   showSpinner();
-  console.log("50 results are searched");
+  db.collection("posts").onSnapshot((querySnapshot) => {
+    removeSpinner();
+    querySnapshot.forEach((doc) => {
+      console.log(doc.data());
+      if (
+        doc
+          .data()
+          .title.toLowerCase()
+          .includes(inputTextField.value.toLowerCase())
+      ) {
+        renderNewPost({ id: doc.id, ...doc.data() });
+        console.log(doc.data());
+      }
 
-  setTimeout(removeSpinner, 1000);
+      /**** ===== Navigate to post detail ===== ****/
+      const postTitle = document.querySelectorAll(".post-title");
+      const postReadmore = document.querySelectorAll(".post-readmore");
+      const postContent = document.querySelectorAll(".post-content");
+
+      function renderNewPost(post) {
+        const postHomePage = document.querySelector("#homepage-content");
+
+        // Container
+        const container = document.createElement("div");
+        container.setAttribute("class", "container-fluid my-3");
+        postHomePage.appendChild(container);
+        container.addEventListener("click", () => {
+          window.location.href = `./post-detail.html?post_id=${post.id}`;
+        });
+
+        // Title
+        const postTitleElem = document.createElement("div");
+        postTitleElem.setAttribute("class", "h4 post-title");
+        postTitleElem.innerText = post.title;
+        container.appendChild(postTitleElem);
+
+        // Content
+        const postContentElem = document.createElement("div");
+        postContentElem.setAttribute("class", "my-2 post-content");
+        const formattedContent = shortenContent(post.content);
+        postContentElem.innerHTML = `
+          <p>${formattedContent}
+            ${
+              formattedContent.length < post.content.length &&
+              `<span class="text-read-more text-primary post-readmore">
+                  Read More
+                </span>`
+            }
+          </p>
+        `;
+        container.appendChild(postContentElem);
+
+        // Separated line
+        const line = document.createElement("div");
+        line.setAttribute("class", "bg-secondary w-100 separated-line");
+        container.parentNode.appendChild(line);
+      }
+      function shortenContent(content) {
+        if (content.length >= 300) {
+          return content.substr(0, 300) + "...";
+        } else {
+          return content;
+        }
+      }
+    });
+  });
 });
