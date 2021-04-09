def121b0942f8320e2fce12d4d8f14a08b319f20
Hyerim_Shin <sinhyerim@gmail.com>
2021-04-09 12:16:08 -0700

    Debugged search.html

--- scripts/search.js
+++ scripts/search.js
@@ -1,22 +1,22 @@
 const searchButton = document.getElementById("search-post");
-var showresult = document.querySelector("#result-here");
-var inputTextField = document.querySelector("#search-result");
+var numberOfResult = document.querySelector("#number-of-result");
+var inputTextField = document.querySelector("#search-keyword");
 var resultNum = 0;
+const searchResultWrapper = document.querySelector("#search-content");
+searchResultWrapper.setAttribute("class", "container-fluid my-3");
 
 /**** ===== Search the Key word =====****/
 searchButton.addEventListener("click", function () {
   showSpinner();
+
   db.collection("posts").onSnapshot((querySnapshot) => {
     removeSpinner();
 
     /**** ===== Search Result ===== ****/
-    let resultNumContainer = document.createElement("div");
-    resultNumContainer.setAttribute("id", "resultNum");
     resultNum = 0;
-    showresult.appendChild(resultNumContainer);
 
+    searchResultWrapper.innerHTML = "";
     querySnapshot.forEach((doc) => {
-      console.log(doc.data());
       if (
         doc
           .data()
@@ -25,39 +25,39 @@ searchButton.addEventListener("click", function () {
       ) {
         renderNewPost({ id: doc.id, ...doc.data() });
         console.log(doc.data());
+
         resultNum++;
       }
 
-      document.getElementById("resultNum").innerText =
-        resultNum + " results are founded";
+      numberOfResult.innerText = resultNum + " results are founded";
+    });
+  });
 
-      /**** ===== Navigate to post detail ===== ****/
-      const postTitle = document.querySelectorAll(".post-title");
-      const postReadmore = document.querySelectorAll(".post-readmore");
-      const postContent = document.querySelectorAll(".post-content");
+  /**** ===== Navigate to post detail ===== ****/
+  // const postTitle = document.querySelectorAll(".post-title");
+  // const postReadmore = document.querySelectorAll(".post-readmore");
+  // const postContent = document.querySelectorAll(".post-content");
 
-      function renderNewPost(post) {
-        const postSearchPage = document.querySelector("#search-content");
+  function renderNewPost(post) {
+    // Container
+    const container = document.createElement("div");
+    container.setAttribute("class", "container-fluid my-3");
+    searchResultWrapper.appendChild(container);
+    container.addEventListener("click", () => {
+      window.location.href = `./post-detail.html?post_id=${post.id}`;
+    });
 
-        // Container
-        const container = document.createElement("div");
-        container.setAttribute("class", "container-fluid my-3");
-        postSearchPage.appendChild(container);
-        container.addEventListener("click", () => {
-          window.location.href = `./post-detail.html?post_id=${post.id}`;
-        });
+    // Title
+    const postTitleElem = document.createElement("div");
+    postTitleElem.setAttribute("class", "h4 post-title");
+    postTitleElem.innerText = post.title;
+    container.appendChild(postTitleElem);
 
-        // Title
-        const postTitleElem = document.createElement("div");
-        postTitleElem.setAttribute("class", "h4 post-title");
-        postTitleElem.innerText = post.title;
-        container.appendChild(postTitleElem);
-
-        // Content
-        const postContentElem = document.createElement("div");
-        postContentElem.setAttribute("class", "my-2 post-content");
-        const formattedContent = shortenContent(post.content);
-        postContentElem.innerHTML = `
+    // Content
+    const postContentElem = document.createElement("div");
+    postContentElem.setAttribute("class", "my-2 post-content");
+    const formattedContent = shortenContent(post.content);
+    postContentElem.innerHTML = `
           <p>${formattedContent}
             ${
               formattedContent.length < post.content.length &&
@@ -67,20 +67,18 @@ searchButton.addEventListener("click", function () {
             }
           </p>
         `;
-        container.appendChild(postContentElem);
+    container.appendChild(postContentElem);
 
-        // Separated line
-        const line = document.createElement("div");
-        line.setAttribute("class", "bg-secondary w-100 separated-line");
-        container.parentNode.appendChild(line);
-      }
-      function shortenContent(content) {
-        if (content.length >= 300) {
-          return content.substr(0, 300) + "...";
-        } else {
-          return content;
-        }
-      }
-    });
-  });
+    // Separated line
+    const line = document.createElement("div");
+    line.setAttribute("class", "bg-secondary w-100 separated-line");
+    container.parentNode.appendChild(line);
+  }
+  function shortenContent(content) {
+    if (content.length >= 300) {
+      return content.substr(0, 300) + "...";
+    } else {
+      return content;
+    }
+  }
 });
